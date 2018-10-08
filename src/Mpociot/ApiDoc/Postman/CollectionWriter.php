0db966bb9bfc6d454d38e620a43bfb6dcf0a7a66
Erick <erick@erickmonteiro.com.br>
2018-10-08 20:39:10 -0300

    - header

--- src/Mpociot/ApiDoc/Postman/CollectionWriter.php
+++ src/Mpociot/ApiDoc/Postman/CollectionWriter.php
@@ -7,61 +7,72 @@ use Illuminate\Support\Collection;
 
 class CollectionWriter
 {
-    /**
-     * @var Collection
-     */
-    private $routeGroups;
 
-    /**
-     * CollectionWriter constructor.
-     *
-     * @param Collection $routeGroups
-     */
-    public function __construct(Collection $routeGroups)
-    {
-        $this->routeGroups = $routeGroups;
-    }
+	/**
+	 * @var Collection
+	 */
+	private $routeGroups;
 
-    public function getCollection()
-    {
-        $collection = [
-            'variables' => [],
-            'info' => [
-                'name' => '',
-                '_postman_id' => Uuid::uuid4()->toString(),
-                'description' => '',
-                'schema' => 'https://schema.getpostman.com/json/collection/v2.0.0/collection.json',
-            ],
-            'item' => $this->routeGroups->map(function ($routes, $groupName) {
-                return [
-                    'name' => $groupName,
-                    'description' => '',
-                    'item' => $routes->map(function ($route) {
-                        return [
-                            'name' => $route['title'] != '' ? $route['title'] : url($route['uri']),
-                            'request' => [
-                                'url' => url($route['uri']),
-                                'method' => $route['methods'][0],
-                                'body' => [
-                                    'mode' => 'formdata',
-                                    'formdata' => collect($route['parameters'])->map(function ($parameter, $key) {
-                                        return [
-                                            'key' => $key,
-                                            'value' => isset($parameter['value']) ? $parameter['value'] : '',
-                                            'type' => 'text',
-                                            'enabled' => true,
-                                        ];
-                                    })->values()->toArray(),
-                                ],
-                                'description' => $route['description'],
-                                'response' => [],
-                            ],
-                        ];
-                    })->toArray(),
-                ];
-            })->values()->toArray(),
-        ];
+	/**
+	 * CollectionWriter constructor.
+	 *
+	 * @param Collection $routeGroups
+	 */
+	public function __construct(Collection $routeGroups)
+	{
+		$this->routeGroups = $routeGroups;
+	}
 
-        return json_encode($collection);
-    }
+	public function getCollection()
+	{
+		$collection = [
+			'variables' => [],
+			'info'      => [
+				'name'        => '',
+				'_postman_id' => Uuid::uuid4()->toString(),
+				'description' => '',
+				'schema'      => 'https://schema.getpostman.com/json/collection/v2.0.0/collection.json',
+			],
+			'item'      => $this->routeGroups->map(function ($routes, $groupName) {
+				return [
+					'name'        => $groupName,
+					'description' => '',
+					'item'        => $routes->map(function ($route) {
+						return [
+							'name'    => $route['title'] != '' ? $route['title'] : url($route['uri']),
+							'request' => [
+								'url'         => url($route['uri']),
+								'method'      => $route['methods'][0],
+								"header"      => [
+									[
+										"key"   => "Accept",
+										"value" => "{{Accept}}",
+									],
+									[
+										"key"   => "Language",
+										"value" => "{{Language}}",
+									],
+								],
+								'body'        => [
+									'mode'     => 'formdata',
+									'formdata' => collect($route['parameters'])->map(function ($parameter, $key) {
+										return [
+											'key'     => $key,
+											'value'   => isset($parameter['value']) ? $parameter['value'] : '',
+											'type'    => 'text',
+											'enabled' => true,
+										];
+									})->values()->toArray(),
+								],
+								'description' => $route['description'],
+								'response'    => [],
+							],
+						];
+					})->toArray(),
+				];
+			})->values()->toArray(),
+		];
+
+		return json_encode($collection);
+	}
 }
