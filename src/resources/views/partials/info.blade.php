0057aa2807a48c61377cdc78e98395f948c95745
Erick <erick@erickmonteiro.com.br>
2018-10-15 14:56:43 -0300

    - Update de documentação

--- src/resources/views/partials/info.blade.php
+++ src/resources/views/partials/info.blade.php
@@ -42,11 +42,12 @@ Error Code | Meaning
 ---------- | -------
 400 | Bad Request -- Your request sucks
 401 | Unauthorized -- Your API key is wrong
-403 | Forbidden -- The kitten requested is hidden for administrators only
-404 | Not Found -- The specified kitten could not be found
-405 | Method Not Allowed -- You tried to access a kitten with an invalid method
-406 | Not Acceptable -- You requested a format that isn't json
-410 | Gone -- The kitten requested has been removed from our servers
-429 | Too Many Requests -- You're requesting too many kittens! Slow down!
+403 | Forbidden -- You are not authorized or do not have permission to access
+404 | Not Found -- The specified page can not be found
+405 | Method Not Allowed -- Method not allowed for this request
+406 | Not Acceptable -- You have requested a format that is not valid
+410 | Gone -- The target resource is no longer available
+413 | Payload Too Large -- Request payload is larger than the server is willing or able to process
+429 | Too Many Requests -- You have sent too many requests in a certain amount of time ("rate limiting")
 500 | Internal Server Error -- We had a problem with our server. Try again later.
 503 | Service Unavailable -- We're temporarially offline for maintanance. Please try again later.
